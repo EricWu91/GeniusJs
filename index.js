bb33d0c9c3ebf247c972686abe6341278a66be0b
Eric Wu <eric.wu@bluefleet.com.br>
2024-10-16 09:52:52 -0300

    Logica if

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 function gerarSequencia()
 {
 	let campoSequencia = document.getElementById('sequencia');
-	let resultado = document.getElementById('resultado');
 	let numeroAleatorio = (gerarAleatorioEntre1E4());
-	resultado.innerHTML = numeroAleatorio;
-	resultado.classList = "borda-"+numeroAleatorio;
+	campoSequencia.value = numeroAleatorio;
+	campoSequencia.classList = "borda-"+numeroAleatorio;
 	
 }
+
+function validarSequencia(){
+	let sequencia = document.getElementById('sequencia').value;
+
+	let valorEntrada = document.getElementById('entrada').value;
+
+
+	if(valorEntrada != sequencia){
+		document.getElementById('resultadoValidacao').value = "Estão diferentes!"
+	}		
+	else{
+		document.getElementById('resultadoValidacao').value = "Estão iguais!"
+
+	}
+}
+
 
 function gerarAleatorioEntre1E4(){
 	let aleatorio = Math.floor(Math.random() * 100) ;
